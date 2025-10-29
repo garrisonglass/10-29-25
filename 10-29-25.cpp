ad25cda2b5979b34e5ec8acc15578380591179c1
garrisonglass <garrison.glass@gmail.com>
2025-10-29 17:59:46 -0400

    not finished

--- 10-29-25.cpp
+++ 10-29-25.cpp
@@ -1,10 +1,19 @@
 // 10-29-25.cpp : 
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main()
 {
+    //for i 0 - 20 inclusive
+    //loop
+    for(int c = 0; c <=20; c++)
+    {
+        //f = 9 / *i+32
+        double f = (9 / 5.0) * c + 32;
+        cout << "F:" << left<< setw(5) << "C:"
+    }
     cout << "Hello World!\n";
     return 0;
 }
